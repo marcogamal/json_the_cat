f11b45f825a5802306e35627cfdc1b6c55baae01
Marco <marco.salman@gmail.com>
2022-02-27 07:16:25 -0500

    refactoring + mocha & chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,38 @@
 const request = require("request");
+const fs = require("fs");
 
-const breedName = process.argv[2]
-const domain = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    "https://api.thecatapi.com/v1/breeds/search?q=siberian",
+    (error, response, body) => {
+      const data = JSON.parse(body);
+      const desc = data[0]["description"];
 
-request(domain, (error, response, body) => {
-  if (error) {
-    console.log("error:", error);
-  }
-const data = JSON.parse(body);
-console.log(data);
-// console.log(typeof data);
-// const description = data[0])
-// console.log('=========')
-// console.log(description)
-})
+      fs.readFile(body, "utf8", () => {
+        if (error) {
+          callback(error, null);
+        }
+        if (data.length === 0) {
+          callback(error, null);
+        } else {
+          callback(null, desc);
+        }
+      });
+    }
+  );
+};
 
+// const breedName = process.argv[2];
+// const domain = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+//   request(domain, (error, response, body) => {
+//     if (error) {
+//       console.log("error:", error);
+//     }
+//     const data = JSON.parse(body);
+//     console.log(data);
+//     console.log(typeof data);
+//   });
+// };
 // example terminal: node breedFetcher.js Bengal
+
+module.exports = { fetchBreedDescription };
